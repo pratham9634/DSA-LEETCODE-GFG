15ee0ffd8b97d1bbc8000e3f87b5ee1336f2f09a
PRATHAM100 <160828246+pratham9634@users.noreply.github.com>
2025-02-12 19:14:20 +0530

    Time: 21 ms (68.07%) | Memory: 63.8 MB (87.26%) - LeetSync

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.cpp
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.cpp
@@ -1,25 +1,39 @@
 class Solution {
 public:
-    int sum(int n){
-        int ans =0;
-        while(n>0){
-            ans += n%10;
+    int digitSum(int n) {
+        int ans = 0;
+        while (n > 0) {
+            ans += n % 10;
             n /= 10;
         }
         return ans;
     }
+
     int maximumSum(vector<int>& nums) {
-        unordered_map<int,vector<int>>mp;
+        unordered_map<int, pair<int, int>> mp;
         int ans = -1;
-        for(int x : nums){
-            int num = sum(x);
-            if(mp.find(num)!=mp.end()){
-                for(int sm :mp[num]){
-                    int m = sm+x;
-                    ans = max(ans,m);
+
+        for (int x : nums) {
+            int sumDigits = digitSum(x);
+            if (mp.find(sumDigits) != mp.end()) {
+                int max1 = mp[sumDigits].first;
+                int max2 = mp[sumDigits].second;
+                
+                if (x > max1) {
+                    max2 = max1;
+                    max1 = x;
+                } else if (x > max2) {
+                    max2 = x;
                 }
+
+                mp[sumDigits] = {max1, max2};
+
+                if (max2 != -1) {
+                    ans = max(ans, max1 + max2);
+                }
+            } else {
+                mp[sumDigits] = {x, -1};
             }
-            mp[num].push_back(x);
         }
         return ans;
     }
