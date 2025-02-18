c27dca15e2aeafd4095eb6deb26fada7570791ef
PRATHAM100 <160828246+pratham9634@users.noreply.github.com>
2025-02-18 18:50:42 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (26.91%) - LeetSync

---
+++ 979-di-string-match/di-string-match.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    
+    vector<int> diStringMatch(string s) {
+       int n = s.length();
+        int low = 0, high = n;
+        vector<int> ans;
+
+        for (char c : s) {
+            if (c == 'I') {
+                ans.push_back(low++);
+            } else { 
+                ans.push_back(high--);
+            }
+        }
+        ans.push_back(low); 
+        return ans;
+    }
+};
