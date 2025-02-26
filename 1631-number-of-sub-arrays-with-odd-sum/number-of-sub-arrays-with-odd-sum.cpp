763919f649e93450eb7143355c5b46da1f6d5d63
PRATHAM100 <160828246+pratham9634@users.noreply.github.com>
2025-02-26 18:42:21 +0530

    Time: 0 ms (100.00%) | Memory: 112 MB (54.77%) - LeetSync

--- 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.cpp
+++ 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.cpp
@@ -1,12 +1,23 @@
 class Solution {
 public:
     int numOfSubarrays(vector<int>& arr) {
-        long long oddCount = 0, prefixSum = 0;
+        long long oc = 0, ec=0,ps= 0;
+        int ans = 0;
         for(int a : arr) {
-            prefixSum += a;
-            oddCount += prefixSum % 2;
+            ps += a;
+            if(ps % 2){
+                oc++;
+                ans++;
+                ans += ec;
+                ans %= 1'000'000'007;
+            }
+            else{
+                ec++;
+                ans += oc;
+                ans %= 1'000'000'007;
+                
+            }
         }
-        oddCount += (arr.size() - oddCount) * oddCount;
-        return oddCount % 1'000'000'007;
+        return ans % 1'000'000'007;
     }
 };
