0cfc3e549d0b1e37d6eaf32512ae131f6b165f31
PRATHAM100 <160828246+pratham9634@users.noreply.github.com>
2025-02-26 20:04:10 +0530

    Time: 32 ms (95.17%) | Memory: 134 MB (90.65%) - LeetSync

--- 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.cpp
+++ 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.cpp
@@ -6,17 +6,15 @@ public:
             pair<int, pair<int, int>>, 
             vector<pair<int, pair<int, int>>>, 
             greater<pair<int, pair<int, int>>> 
-        > pq; // Min-heap
+        > pq; 
         
         int n1 = nums1.size(), n2 = nums2.size();
         if (n1 == 0 || n2 == 0) return ans;
 
-        // Step 1: Push first k pairs (nums1[i], nums2[0])
         for (int i = 0; i < min(n1, k); i++) {
             pq.push({nums1[i] + nums2[0], {i, 0}});
         }
 
-        // Step 2: Extract k smallest pairs
         while (k-- > 0 && !pq.empty()) {
             auto p = pq.top();
             pq.pop();
@@ -24,7 +22,6 @@ public:
 
             ans.push_back({nums1[i], nums2[j]});
 
-            // If there's a next pair (i, j+1), push it into the heap
             if (j + 1 < n2) {
                 pq.push({nums1[i] + nums2[j + 1], {i, j + 1}});
             }
