26ce20347faa30aa7f7e478b764d9c8dc60cf3cd
PRATHAM100 <160828246+pratham9634@users.noreply.github.com>
2025-03-01 17:59:33 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (23.58%) - LeetSync

---
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> applyOperations(vector<int>& nums) {
+    for(int i = 0; i+1 < nums.size(); ++i){
+        if(nums[i] == nums[i+1]){         
+            nums[i] = 2*nums[i]; 
+            nums[i+1] = 0;
+        }
+    }
+    int i = 0;
+    for(auto n: nums){                   
+        if(n != 0) nums[i++] = n;
+    }
+    while(i < nums.size()) nums[i++] = 0; 
+    return nums;
+}
+};
