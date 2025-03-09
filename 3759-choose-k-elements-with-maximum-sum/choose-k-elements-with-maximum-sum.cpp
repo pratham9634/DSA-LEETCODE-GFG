dfbf3bcbf4a2da1e01bef1696341e5aed39f9d0a
PRATHAM100 <160828246+pratham9634@users.noreply.github.com>
2025-03-09 21:09:45 +0530

    Time: 196 ms (37.50%) | Memory: 194.5 MB (25.00%) - LeetSync

---
+++ 3759-choose-k-elements-with-maximum-sum/choose-k-elements-with-maximum-sum.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<long long> findMaxSum(vector<int>& nums1, vector<int>& nums2, int k) {
+        int n = nums1.size();
+        vector<long long> ans(n, 0); 
+        vector<pair<int, int>> idx;
+        for (int i = 0; i < n; i++) 
+            idx.push_back({nums1[i], i});
+        
+        sort(idx.begin(), idx.end());
+        
+        priority_queue<int, vector<int>, greater<int>> pq; \
+        unordered_map<int, long long> mp; 
+        long long sum = 0;
+        int j = 0; 
+        
+        for (int i = 0; i < n; i++) {
+            int index = idx[i].second; 
+            while (j < i) {
+                int prev = idx[j].second; 
+                if (idx[j].first >= idx[i].first) break;
+
+                pq.push(nums2[prev]);
+                sum += nums2[prev];
+
+\
+                if (pq.size() > k) {
+                    sum -= pq.top();
+                    pq.pop();
+                }
+                j++;
+            }
+            
+            mp[index] = sum; 
+        }
+        
+
+        for (int i = 0; i < n; i++)
+            ans[i] = mp[i];
+
+        return ans;
+    }
+};
