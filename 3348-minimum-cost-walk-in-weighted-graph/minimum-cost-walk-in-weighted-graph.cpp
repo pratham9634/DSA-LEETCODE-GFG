1bfe12567eafd0b4ed7e6673658c911ac827eda1
PRATHAM100 <160828246+pratham9634@users.noreply.github.com>
2025-03-20 18:46:03 +0530

    Time: 20 ms (70.45%) | Memory: 207.9 MB (66.56%) - LeetSync

--- 3348-minimum-cost-walk-in-weighted-graph/minimum-cost-walk-in-weighted-graph.cpp
+++ 3348-minimum-cost-walk-in-weighted-graph/minimum-cost-walk-in-weighted-graph.cpp
@@ -1,40 +1,48 @@
+#include <vector>
+using namespace std;
+
 class Solution {
 public:
-    vector<int> minimumCost(int n, vector<vector<int>>& edges, vector<vector<int>>& query) {
-        vector<int> parent(n), min_path_cost(n, -1);
-        iota(parent.begin(), parent.end(), 0);
+    vector<int> parent, minPathCost;
 
-        function<int(int)> find_root = [&](int node) {
-            if (parent[node] != node) {
-                parent[node] = find_root(parent[node]);
-            }
-            return parent[node];
-        };
+    int findRoot(int node) {
+        if (parent[node] != node) {
+            parent[node] = findRoot(parent[node]); // Path compression
+        }
+        return parent[node];
+    }
+
+    vector<int> minimumCost(int n, vector<vector<int>>& edges, vector<vector<int>>& queries) {
+        parent.resize(n);
+        minPathCost.assign(n, -1);
+        for (int i = 0; i < n; i++) parent[i] = i;
 
         for (auto& edge : edges) {
             int source = edge[0], target = edge[1], weight = edge[2];
-            int source_root = find_root(source);
-            int target_root = find_root(target);
+            int sourceRoot = findRoot(source);
+            int targetRoot = findRoot(target);
 
-            min_path_cost[target_root] &= weight;
+            if (minPathCost[sourceRoot] == -1) minPathCost[sourceRoot] = weight;
+            else minPathCost[sourceRoot] &= weight;
 
-            if (source_root != target_root) {
-                min_path_cost[target_root] &= min_path_cost[source_root];
-                parent[source_root] = target_root;
+            if (minPathCost[targetRoot] == -1) minPathCost[targetRoot] = weight;
+            else minPathCost[targetRoot] &= weight;
+
+            if (sourceRoot != targetRoot) {
+                parent[sourceRoot] = targetRoot;
+                minPathCost[targetRoot] &= minPathCost[sourceRoot];
             }
         }
 
         vector<int> result;
-        for (auto& q : query) {
+        for (auto& q : queries) {
             int start = q[0], end = q[1];
-            if (start == end) {
-                result.push_back(0);
-            } else if (find_root(start) != find_root(end)) {
-                result.push_back(-1);
-            } else {
-                result.push_back(min_path_cost[find_root(start)]);
-            }
+
+            if (start == end) result.push_back(0);
+            else if (findRoot(start) != findRoot(end)) result.push_back(-1);
+            else result.push_back(minPathCost[findRoot(start)]);
         }
+
         return result;
     }
 };
