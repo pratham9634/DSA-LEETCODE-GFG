287f1227c565585139c8dd5764d24146e10cb48a
PRATHAM100 <160828246+pratham9634@users.noreply.github.com>
2025-04-04 12:36:39 +0530

    Time: 0 ms (100.00%) | Memory: 16.3 MB (32.46%) - LeetSync

---
+++ 896-smallest-subtree-with-all-the-deepest-nodes/smallest-subtree-with-all-the-deepest-nodes.cpp
@@ -0,0 +1,14 @@
+
+class Solution {
+public:
+    pair<int, TreeNode*> dfs(TreeNode *root) {
+    if (!root) return {0, nullptr};
+    auto [d1, r1] = dfs(root->left);
+    auto [d2, r2] = dfs(root->right);
+    return {max(d1, d2) + 1, d1 == d2 ? root : d1 > d2 ? r1 : r2};
+}
+
+TreeNode* subtreeWithAllDeepest(TreeNode* root) {
+    return dfs(root).second;
+}
+};
