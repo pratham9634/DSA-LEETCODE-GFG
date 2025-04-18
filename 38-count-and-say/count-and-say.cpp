62428babb1130a54f12ef504f61cbc89ffb0f15f
PRATHAM100 <160828246+pratham9634@users.noreply.github.com>
2025-04-19 03:10:47 +0530

    Time: 3 ms (83.82%) | Memory: 9.9 MB (60.76%) - LeetSync

--- 38-count-and-say/count-and-say.cpp
+++ 38-count-and-say/count-and-say.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
     
-    string RLE(string s, int n){
-        
-        if(n == 1){
-            return s;
-        }
-        int i=0;
-        string ans = "";
-        int j=0;
-        while(i<s.length()){ 
-           int count = 0; 
-          while(j<s.length() && s[i] == s[j]){
-            j++;
-          }
-         count = j-i;
-          ans += to_string(count);
-          ans.push_back(s[i]);
-          i=j;
-        }
-        n--;
-        return RLE(ans, n);
-    }
+
     string countAndSay(int n) {
-        if(n == 1) return "1";
-        string s ="1";
-        return RLE(s,n);
+        if (n == 1) return "1";
+        string s = "1";
+        
+        for (int i = 1; i < n; i++) { 
+            string a = "";
+            int c = 1;
+            
+            for (int j = 1; j < s.length(); j++) {
+                if (s[j] == s[j - 1]) {
+                    c++;
+                } else {
+                    a += to_string(c);
+                    a.push_back(s[j - 1]);
+                    c = 1;
+                }
+            }
+            a += to_string(c);         
+            a.push_back(s.back());    
+            s = a;
+        }
+        
+        return s;
     }
 };
