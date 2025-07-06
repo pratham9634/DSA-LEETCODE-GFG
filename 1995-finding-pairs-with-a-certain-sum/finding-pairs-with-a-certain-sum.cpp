79ab2e91812674de883871b3036aa2495381f7e5
PRATHAM100 <160828246+pratham9634@users.noreply.github.com>
2025-07-06 12:22:44 +0530

    Time: 147 ms (48.02%) | Memory: 132.8 MB (36.74%) - LeetSync

---
+++ 1995-finding-pairs-with-a-certain-sum/finding-pairs-with-a-certain-sum.cpp
@@ -0,0 +1,33 @@
+class FindSumPairs {
+public:
+    vector<int> v1, v2;
+    unordered_map<int, int> m;
+    
+    FindSumPairs(vector<int>& nums1, vector<int>& nums2) {
+        v1 = nums1;
+        v2 = nums2;
+        sort(v1.begin(), v1.end());
+        
+        int n = nums2.size();
+        for (int i = 0; i < n; i++)
+            m[nums2[i]]++;
+    }
+    
+    void add(int index, int val) {
+        m[v2[index]]--;
+        v2[index] += val;
+        m[v2[index]]++;
+    }
+    
+    int count(int tot) {
+        int ans = 0;
+        int n1 = v1.size();
+        
+        for (int i = 0; i < n1; i++) {
+            if (v1[i] >= tot)
+                break;
+            ans += m[tot - v1[i]];
+        }
+        return ans;
+    }
+};
