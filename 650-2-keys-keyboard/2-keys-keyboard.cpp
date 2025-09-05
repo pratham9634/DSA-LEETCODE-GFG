9cf3cf8ae45d207b23a4b41837f3daa75ccb5d58
PRATHAM100 <160828246+pratham9634@users.noreply.github.com>
2025-09-05 12:24:02 +0530

    Time: 14 ms (34.98%) | Memory: 12.4 MB (24.37%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/2-keys-keyboard.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
+    int t[1001][1001];
+    int solve(int curr,int clip,int n){
+        if(curr==n) return 0;
+        if(curr>n) return 100000;
+        if(t[curr][clip]!=-1) return t[curr][clip];
+        int cp = 1+1+solve(curr+curr,curr,n);
+        int p = 1+solve(curr+clip,clip,n);
+
+        return t[curr][clip]=min(cp,p);
+    }
     int minSteps(int n) {
-        if (n == 1) return 0;
-        
-        int steps = 0;
-        int factor = 2;
-        
-        while (n > 1) {
-            while (n % factor == 0) {
-                steps += factor;
-                n /= factor;
-            }
-            factor++;
-        }
-        
-        return steps;
+        if(n==1) return 0;
+        memset(t,-1,sizeof(t));
+        return 1+solve(1,1,n);
     }
 };
