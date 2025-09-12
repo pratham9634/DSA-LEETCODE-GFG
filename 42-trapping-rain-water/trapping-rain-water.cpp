3371d544fa3a31bb000e27f09c9559367f5af515
PRATHAM100 <160828246+pratham9634@users.noreply.github.com>
2025-09-12 14:02:09 +0530

    Time: 3 ms (18.03%) | Memory: 27.1 MB (39.72%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -2,24 +2,22 @@ class Solution {
 public:
     int trap(vector<int>& h) {
         int n = h.size();
-        if(n == 0) return 0;
-
-        vector<int> l(n, 0);
-        vector<int> r(n, 0);
-
-        l[0] = h[0];
-        for(int i = 1; i < n; i++) {
-            l[i] = max(l[i - 1], h[i]);
-        }
-
-        r[n - 1] = h[n - 1];
-        for(int i = n - 2; i >= 0; i--) {
-            r[i] = max(r[i + 1], h[i]);
-        }
-
         int ans = 0;
+        stack<int> st;
+
         for(int i = 0; i < n; i++) {
-            ans += max(0, min(l[i], r[i]) - h[i]);
+            while(!st.empty() && h[i] > h[st.top()]) {
+                int top = st.top();
+                st.pop();
+
+                if(st.empty())
+                    break;
+
+                int distance = i - st.top() - 1;
+                int bounded_height = min(h[i], h[st.top()]) - h[top];
+                ans += distance * bounded_height;
+            }
+            st.push(i);
         }
 
         return ans;
