0d0f8eea1bc6e96ec89e2e6441ea50625b133388
PRATHAM <160828246+pratham9634@users.noreply.github.com>
2026-03-26 16:55:10 +0530

    Time: 904 ms (60.00%) | Memory: 389.4 MB (58.46%) - LeetSync

---
+++ 3850-equal-sum-grid-partition-ii/equal-sum-grid-partition-ii.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    typedef long long ll;
+    ll total = 0;
+    bool check(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        unordered_set<ll> st;
+        ll top = 0;
+
+        for(int i = 0; i <= m-2; i++) {
+
+            for(int j = 0; j < n; j++) {
+                st.insert(grid[i][j]);
+                top += grid[i][j];
+            }
+
+            ll bottom = (total - top);
+            ll diff = top - bottom;
+
+            if (diff == 0) return true;
+
+            if (diff == (ll)grid[0][0])   return true;
+            if (diff == (ll)grid[0][n-1]) return true;
+            if (diff == (ll)grid[i][0]) return true;
+
+            if(i > 0 && n > 1 && st.count(diff)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+    bool canPartitionGrid(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                total += grid[i][j];
+            }
+        }
+
+        if(check(grid)) return true;
+        reverse(begin(grid),end(grid));
+        if(check(grid)) return true;
+        reverse(begin(grid),end(grid));
+        vector<vector<int>>tr(n,vector<int>(m,0));
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                tr[j][i] = grid[i][j];
+            }
+        }
+        if(check(tr)) return true;
+        reverse(begin(tr),end(tr));
+        if(check(tr)) return true;
+
+        return false;
+    }
+};
