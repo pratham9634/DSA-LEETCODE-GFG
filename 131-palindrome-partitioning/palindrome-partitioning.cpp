e7e09715b47c348300a231dfd6a8735d646763de
PRATHAM <160828246+pratham9634@users.noreply.github.com>
2026-04-10 14:14:55 +0530

    Time: 62 ms (27.68%) | Memory: 61.7 MB (43.67%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int n;
-    bool ip(string &s,int i,int j){
+    bool helper(string s,int i,int j){
         while(i<j){
             if(s[i]!=s[j]) return false;
             i++;
@@ -9,16 +9,16 @@ public:
         }
         return true;
     }
-    void solve(string &s,int idx, vector<string> curr,vector<vector<string>>&ans){
+    void solve(int idx,string s,vector<string>&curr,vector<vector<string>>&ans){
+
         if(idx==n){
             ans.push_back(curr);
             return;
         }
-
         for(int i=idx;i<n;i++){
-            if(ip(s,idx,i)){
+            if(helper(s,idx,i)){
                 curr.push_back(s.substr(idx,i-idx+1));
-                solve(s,i+1,curr,ans);
+                solve(i+1,s,curr,ans);
                 curr.pop_back();
             }
         }
@@ -26,10 +26,8 @@ public:
     vector<vector<string>> partition(string s) {
         n = s.length();
         vector<vector<string>>ans;
-        vector<string> curr;
-
-        solve(s,0,curr,ans);
-
+        vector<string>curr;
+        solve(0,s,curr,ans);
         return ans;
     }
 };
