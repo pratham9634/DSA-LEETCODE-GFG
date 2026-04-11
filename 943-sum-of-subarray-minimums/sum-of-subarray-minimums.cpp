b64c7714e80185421ab5400bda7a41ad185a01c2
PRATHAM <160828246+pratham9634@users.noreply.github.com>
2026-04-11 14:35:42 +0530

    Time: 14 ms (71.42%) | Memory: 47.2 MB (57.45%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -3,29 +3,28 @@ public:
     int mod = 1e9+7;
     int sumSubarrayMins(vector<int>& arr) {
         int n = arr.size();
-        vector<int> l(n,-1);
-        vector<int> r(n,n);
-        stack<int> st;
+        vector<int>ps(n,-1);
+        vector<int>ns(n,n);
+
+        stack<int>st;
 
         for(int i=0;i<n;i++){
-            while(!st.empty()&& arr[st.top()]>=arr[i])st.pop();
-            if(!st.empty()) l[i] = st.top();
+            while(!st.empty() && arr[i]<=arr[st.top()]) st.pop();
+            if(!st.empty()) ps[i] = st.top();
             st.push(i);
         }
-
         st = stack<int>();
-
         for(int i=n-1;i>=0;i--){
-            while(!st.empty()&& arr[st.top()]>arr[i])st.pop();
-            if(!st.empty()) r[i] = st.top();
+            while(!st.empty() && arr[i]<arr[st.top()]) st.pop();
+            if(!st.empty()) ns[i] = st.top();
             st.push(i);
         }
-
-        long long sum =0;
+        long long ans = 0;
         for(int i=0;i<n;i++){
-            sum += static_cast<long long>(i-l[i])*(r[i]-i)*arr[i] % mod;
-            sum %= mod;
+            ans += static_cast<long long>(i-ps[i])*(ns[i]-i)*arr[i];
+            ans %= mod;
         }
-        return sum;
+        return ans;
+
     }
 };
